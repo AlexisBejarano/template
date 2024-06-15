1a62207ef829afadfb55e3de9585b2bce1430411
Alexis Bejarano <aletzisbejaranov@hotmail.com>
2024-06-15 02:02:09 -0700

    Se cambiaron los div a img correpondientes.

--- app/components/Navegation.js
+++ app/components/Navegation.js
@@ -5,7 +5,7 @@ export default function Navegation() {
             <div className="flex w-full">
 
                 <div className="flex items-center">
-                    <div className="w-9 h-9 bg-red-700"></div>
+                    <img src="#" className="w-9 h-9 bg-red-700"/>
                     <div className="mx-3 font-bold text-sky-900">intellectus</div>
                     <input type="search" id="#" className="rounded-md h-6" />
                 </div>
@@ -13,14 +13,14 @@ export default function Navegation() {
                 <div className="flex ml-auto items-center">
                     {['Core', 'Descubrimiento', 'Webint', 'Registro de llamadas'].map((item, index) => (
                         <div key={index} className="flex items-center mx-1 cursor-pointer">
-                            <div className="w-6 h-6 bg-red-700 opacity-10"></div>
+                            <img src="#" className="w-6 h-6 bg-red-700"/>
                             <div className="text-xs mx-1">{item}</div>
                         </div>
                     ))}
                     <div className="border border-black opacity-20 mx-2"></div>
                     <div className="flex items-center">
                         {[...Array(5)].map((_, index) => (
-                            <div key={index} className="w-7 h-7 mx-2 bg-red-700 opacity-10 rounded-xl cursor-pointer"></div>
+                            <img src="#" key={index} className="w-7 h-7 mx-2 bg-red-700 rounded-xl cursor-pointer"/>
                         ))}
                     </div>
                 </div>
--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -2,14 +2,14 @@ export default function Sidebar() {
     return (
         <div className="w-max h-screen drop-shadow-lg bg-slate-100">
             <div className="bg-slate-400 h-10 flex items-center px-4">
-                <p>Log</p>
+                <div className="font-semibold">Log</div>
                 <div className="flex ml-auto">
-                    <div className="w-6 h-6 bg-black mx-2"></div>
-                    <div className="w-6 h-6 bg-black mx-2"></div>
-                    <div className="w-6 h-6 bg-black mx-2"></div>
+                    <img src="#" className="w-6 h-6 bg-black mx-2"/>
+                    <img src="#" className="w-6 h-6 bg-black mx-2"/>
+                    <img src="#" className="w-6 h-6 bg-black mx-2"/>
                     <input type="search" id="#" className="rounded-md h-6 w-48" />
-                    <div className="w-6 h-6 bg-black mx-2"></div>
-                    <div className="w-6 h-6 bg-black ml-2"></div>
+                    <img src="#" className="w-6 h-6 bg-black mx-2"/>
+                    <img src="#" className="w-6 h-6 bg-black mL-2"/>
                 </div>
             </div>
 
@@ -23,73 +23,73 @@ export default function Sidebar() {
                         <th className="px-2">Fecha</th>
                         <th className="px-2">Tipo</th>
                         <th className="px-2">Acciones</th>
-                        <th className="px-2"><div className="w-6 h-6 bg-black"></div></th>
+                        <th className="px-2"><img src="#" className="w-6 h-6 bg-black"/></th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="border-b border-gray-600 h-10">
+                    <tr className="border-b cursor-pointer border-gray-600 h-10">
                         <td className="px-2"><input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" /></td>
                         <td className="px-2">+5233142766</td>
                         <td className="px-2">04.06.2024 22:56</td>
                         <td className="px-2"></td>
-                        <td className="px-2"><div className="w-6 h-6 mx-auto bg-black"></div></td>
-                        <td className="px-2"><div className="w-6 h-6 bg-black"></div></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
                     </tr>
-                    <tr className="border-b border-gray-600 h-10">
+                    <tr className="border-b cursor-pointer border-gray-600 h-10">
                         <td className="px-2"><input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" /></td>
                         <td className="px-2">+5233142766</td>
                         <td className="px-2">04.06.2024 22:56</td>
                         <td className="px-2"></td>
-                        <td className="px-2"><div className="w-6 h-6 mx-auto bg-black"></div></td>
-                        <td className="px-2"><div className="w-6 h-6 bg-black"></div></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
                     </tr>
-                    <tr className="border-b border-gray-600 h-10">
+                    <tr className="border-b cursor-pointer border-gray-600 h-10">
                         <td className="px-2"><input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" /></td>
                         <td className="px-2">+5233142766</td>
                         <td className="px-2">04.06.2024 22:56</td>
                         <td className="px-2"></td>
-                        <td className="px-2"><div className="w-6 h-6 mx-auto bg-black"></div></td>
-                        <td className="px-2"><div className="w-6 h-6 bg-black"></div></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
                     </tr>
-                    <tr className="border-b border-gray-600 h-10">
+                    <tr className="border-b cursor-pointer border-gray-600 h-10">
                         <td className="px-2"><input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" /></td>
                         <td className="px-2">+5233142766</td>
                         <td className="px-2">04.06.2024 22:56</td>
                         <td className="px-2"></td>
-                        <td className="px-2"><div className="w-6 h-6 mx-auto bg-black"></div></td>
-                        <td className="px-2"><div className="w-6 h-6 bg-black"></div></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
                     </tr>
-                    <tr className="border-b border-gray-600 h-10">
+                    <tr className="border-b cursor-pointer border-gray-600 h-10">
                         <td className="px-2"><input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" /></td>
                         <td className="px-2">+5233142766</td>
                         <td className="px-2">04.06.2024 22:56</td>
                         <td className="px-2"></td>
-                        <td className="px-2"><div className="w-6 h-6 mx-auto bg-black"></div></td>
-                        <td className="px-2"><div className="w-6 h-6 bg-black"></div></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
                     </tr>
-                    <tr className="border-b border-gray-600 h-10">
+                    <tr className="border-b cursor-pointer border-gray-600 h-10">
                         <td className="px-2"><input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" /></td>
                         <td className="px-2">+5233142766</td>
                         <td className="px-2">04.06.2024 22:56</td>
                         <td className="px-2"></td>
-                        <td className="px-2"><div className="w-6 h-6 mx-auto bg-black"></div></td>
-                        <td className="px-2"><div className="w-6 h-6 bg-black"></div></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
                     </tr>
-                    <tr className="border-b border-gray-600 h-10">
+                    <tr className="border-b cursor-pointer border-gray-600 h-10">
                         <td className="px-2"><input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" /></td>
                         <td className="px-2">+5233142766</td>
                         <td className="px-2">04.06.2024 22:56</td>
                         <td className="px-2"></td>
-                        <td className="px-2"><div className="w-6 h-6 mx-auto bg-black"></div></td>
-                        <td className="px-2"><div className="w-6 h-6 bg-black"></div></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
                     </tr>
-                    <tr className="border-b border-gray-600 h-10">
+                    <tr className="border-b cursor-pointer border-gray-600 h-10">
                         <td className="px-2"><input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" /></td>
                         <td className="px-2">+5233142766</td>
                         <td className="px-2">04.06.2024 22:56</td>
                         <td className="px-2"></td>
-                        <td className="px-2"><div className="w-6 h-6 mx-auto bg-black"></div></td>
-                        <td className="px-2"><div className="w-6 h-6 bg-black"></div></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
+                        <td className="px-2"><img src="#" className="w-6 h-6 mx-auto bg-black"/></td>
                     </tr>
 
 
